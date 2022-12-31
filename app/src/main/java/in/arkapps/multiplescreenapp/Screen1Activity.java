ee42ea448749e8d5d1205c312ea955ae0491e089
Zalqarnain <zalqarnainahsen03@gmail.com>
2023-01-01 00:59:43 +0500

    first

---
+++ app/src/main/java/in/arkapps/multiplescreenapp/Screen1Activity.java
@@ -0,0 +1,14 @@
+package in.arkapps.multiplescreenapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Screen1Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_screen1);
+    }
+}
